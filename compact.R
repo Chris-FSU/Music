b0c98202366938e9ad12d71e697562a7c29a0250
Chris Main <cbm19e@my.fsu.edu>
2019-12-28 17:27:25 -0500

    blegh
    (insert non-descriptive commentary here)

--- compact.R
+++ compact.R
@@ -1,10 +1,11 @@
 # This is an attempt to make a more compressed file format for the notes data.
 
 library(audio)
+library(numbers)
 
 # The higher this number, the more crisp everything will be, but the more info it will take up.
 # It needs to be a power of 2 times 10 so that the note names will be correct.
-x <- 320 
+x <- 20 
 
 F <- sin(1:(2*pi*x) / x)
 rat <- 2^((1:12)/12)
@@ -24,15 +25,29 @@ playnote <- function (note,oct,dur) {
   play(rep(note[(1:(length(note)/(2^oct)))*2^oct],dur*2^oct))
 }
 
+extend <- function(vec) {
+  new.vec <- NA
+  new.vec[1:length(vec)*2] <- vec
+  new.vec[(1:length(vec)*2-1)] <- vec
+  return(new.vec)
+}
+# If I could get that to run quickly, I could use it to drop octaves
+
 #Examples
-playnote(C,3,40)
-playnote(E,3,40)
-playnote(G,4,40)
-playnote(C,4,40)
-playnote(E,4,40)
+playnote(C,1,400)
+playnote(E,1,400)
+playnote(G,2,400)
+playnote(C,2,400)
+playnote(E,2,400)
 
-# This is the highest note I can hear:
-playnote(E,8,40)
+# Testing playing multiple waves
+playchord <- function(noteA,noteB) {
+  thing <- (rep(noteA,LCM(length(noteA),length(noteB))/length(noteA)) +
+              rep(noteB,LCM(length(noteB),length(noteA))/length(noteB)))/2
+  play(thing)
+}
 
-# I can't hear this one:
-playnote(F,9,40)
+
+playchord(A,D)
+playchord(B,G)
+playchord(D,B)
--- tiniest.R
+++ tiniest.R
@@ -1,11 +1,18 @@
 # The tiniest format
 library(audio)
+library(numbers)
 
 # This creates a zigzagged line with the appropriate length and a
 # standardised amplitude.
 zigzag <- function(len) {
-  temp <- (c(0:(len/4),(len/4-1):-(len/4),-(len/4-1):-1))/(len/4)
+  temp <- (c(0:(len),(len-1):-(len),-(len-1):-1))/(len)
   return(temp)
 }
 
-play(rep(zigzag(100),40))
+play(rep(zigzag(40),200))
+play(rep(zigzag(50),200))
+play(rep(zigzag(60),200))
+
+# This makes sounds like a 1980s video game.
+# That's cool, but it's not what I want.
+# I think the more natural sound will require more bitspace.
