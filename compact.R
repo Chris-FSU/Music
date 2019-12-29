22a8022d6f3834a02edd9e36eee8abf9cb0e192c
Chris Main <cbm19e@my.fsu.edu>
2019-12-29 10:34:55 -0500

    Update compact.R

--- compact.R
+++ compact.R
@@ -5,8 +5,9 @@ library(numbers)
 
 # The higher this number, the more crisp everything will be, but the more info it will take up.
 # It needs to be a power of 2 times 10 so that the note names will be correct.
-x <- 20 
+x <- 40 
 
+# I avoid calling notes sharps (i.e. F#) because the number sign comments the line.
 F <- sin(1:(2*pi*x) / x)
 rat <- 2^((1:12)/12)
 Gb <- sin(1:(2*pi*x/rat[1]) * rat[1] / x)
@@ -25,27 +26,25 @@ playnote <- function (note,oct,dur) {
   play(rep(note[(1:(length(note)/(2^oct)))*2^oct],dur*2^oct))
 }
 
-extend <- function(vec) {
-  new.vec <- NA
-  new.vec[1:length(vec)*2] <- vec
-  new.vec[(1:length(vec)*2-1)] <- vec
-  return(new.vec)
-}
-# If I could get that to run quickly, I could use it to drop octaves
-
 #Examples
-playnote(C,1,400)
-playnote(E,1,400)
-playnote(G,2,400)
-playnote(C,2,400)
-playnote(E,2,400)
+playnote(C,-1,400) 
+playnote(E,-1,400) 
+playnote(G,0,400) 
+playnote(C,0,400) 
+playnote(E,0,400) 
 
-# Testing playing multiple waves
-playchord <- function(noteA,noteB,noteC,dur) {
+#Testing playing multiple waves
+playchord <- function(noteA,noteB,noteC,dur,oct) {
   thing <- (rep(noteA,LCM(length(noteA),length(noteB))/length(noteA)) +
               rep(noteB,LCM(length(noteB),length(noteA))/length(noteB)))/2
   chord <- (rep(thing,LCM(length(thing),length(noteC))/length(thing)) +
               rep(noteC,LCM(length(noteC),length(thing))/length(noteC)))/2
+  # Get on the right octave
+  while (oct > 1) {
+    chord <- chord[(1:length(chord))*2 -1]
+    oct <- oct - 1
+  }
+  # Copy and/or cut to specified length
   while(length(chord) < dur*25000){
     chord <- rep(chord,2)
   }
@@ -54,9 +53,6 @@ playchord <- function(noteA,noteB,noteC,dur) {
 }
 
 # Test: D major
-# Argument dur is in beats at 120 BPM. dur = 2 is one second.
+# Argument dur is measured in beats at 120 BPM. dur = 2 is one second.
 # I measured this jankily, so it will need precision later.
-playchord(A,D,Gb,2)
-
-
-
+playchord(A,D,Gb,dur=2,oct =2)
